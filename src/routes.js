fe2b220e002e19731d3eaa03c35a4af6d21744d0
Kevin Pessetto <kjpessetto@gmail.com>
2019-07-12 16:02:37 -0500

    Add routes to route.js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,16 @@
-asdf
+import React from 'react';
+import Register from './Components/Register'
+import Home from './Components/Home'
+import WOS from './Components/WOS'
+import Reviews from './Components/Reviews'
+import {Route, Switch} from 'react-router-dom';
+
+export default (
+    <Switch>
+        <Route path='/' exact component={Landing}/>
+        <Route path='/register' component={Register} />
+        <Route path='/home' component={Home} />
+        <Route path='/WOS' component={WOS}/>
+        <Route path='/reviews' component={Reviews}/>
+    </Switch>
+)
