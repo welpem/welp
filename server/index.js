c8fbc746a352b8fb570ed12c8f2dd0d679cc51c9
Rachel Tunon <rachel.tunonn@gmail.com>
2019-07-12 16:09:33 -0500

    add server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
-asdf
+let {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
+
+require('dotenv').config();
+const express = require('express');
+const massive= require('massive');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 3
+    }
+
+})
+)
+
+//database connected//
+massive(CONNECTION_STRING).then(db => {
+    app.set("db", db);
+    console.log("Database connected");
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log("Server listening on port 5050")
+})
