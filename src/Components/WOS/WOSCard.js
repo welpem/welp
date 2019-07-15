149a1e670228f6cfe118702d57a9cf7822c1d1f8
Kevin Pessetto <kjpessetto@gmail.com>
2019-07-15 12:30:17 -0500

    add sql, controller, card, page components for wall of shame

---
+++ src/Components/WOS/WOSCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './WOS.css';
+
+
+
+export default function WOSCard(props) {
+
+    return (
+        <div>
+  
+
+{/* delete button */}
+          <Link to='/wos' onClick={() => props.deleteWOS(props.wos_id)}>
+              <button  aria-label="Delete" >
+                Delete
+              </button>
+          </Link>
+   
+  
+{/* edit button */}
+            {/* <EditFormModule getWOS={props.getWOS} welp_wos={props.welp_wos}/> */}
+  
+
+
+{/* see stuff */}
+            <h1>{props.wos_title}</h1>
+            <img src={props.wos_img} alt="Offender's mugshot" />
+            <p>{props.wos_description}</p>
+
+      </div>
+    );
+  }
