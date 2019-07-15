9cb19e4812876c268aa3cd8490cfbf64048271ca
Mark Nagel <mark_nagel@hotmail.com>
2019-07-15 09:09:23 -0500

    made functional components

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+
+
+function Home() {
+  return (
+    <div className="home">
+        Home Component
+    </div>
+  );
+}
+
+export default Home;
